dc095cf85d2051a8dff7bcfd162af40938f12b42
rivercity310 <h970126@gmail.com>
2025-01-09 16:11:38 +0900

    feat: 이미지 파일 캐싱 설정

--- src/shared/config/env.ts
+++ src/shared/config/env.ts
@@ -1,6 +1,6 @@
 export const ENV = Object.freeze({
   BASE_URL: import.meta.env.VITE_BASE_URL,
   API_URL: import.meta.env.VITE_API_URL,
-  STORAGE_URL: `${import.meta.env.VITE_BASE_URL}/uploads`,
+  STORAGE_URL: `${import.meta.env.VITE_BASE_URL}/storage`,
   LOGO_TEXT: "살구월드"
 });
